a4f63c7349f72a9f43aec860decc288793291dae
Vishal Murudkar <vishalmurudkar24@outlook.com>
2025-05-26 22:44:27 +0530

    Added Swagger authorization for jwt

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 
 namespace Hospital_Management
@@ -21,7 +22,39 @@ namespace Hospital_Management
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+                // Define the security scheme
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT token in the format: Bearer {your token}"
+                });
+
+                // Apply the security scheme globally
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<IPatientService, PatientService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
--- Services/IServices/IAuthService.cs
+++ Services/IServices/IAuthService.cs
@@ -6,6 +6,7 @@ namespace Hospital_Management.Services.IServices
     public interface IAuthService
     {
         public Task<string> Login(LoginRequest model);
+        //public Task<string> RegisterFirstAdmin(RegisterRequest model);
         public Task<string> RegisterAdmin(RegisterRequest model);
         public Task<string> RegisterReceptionist(RegisterRequest model);
         public Task<string> RegisterDoctor(RegisterDoctorRequest model);
